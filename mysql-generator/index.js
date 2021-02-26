c9be4a3a56f2f0e1b4c19ad70a76549554e5f08b
lenchevskii <dima.lenchevskii@gmail.com>
2021-02-26 16:25:23 +0300

    clean start

--- mysql-generator/index.js
+++ mysql-generator/index.js
@@ -1,39 +1,37 @@
-const { generateMatrix, checkDecade }  = require('./source/utils/generator')
+const { generateMatrix
+      , checkDecade 
+      }                   = require('./source/utils/generator')
 
 const { connection }      = require('./source/utils/db')
 
-const { floor }           = require('lodash')
-
 const { trace
       , fNTimes
-      , traceWrap
-      , timerWrap
-      , fNTimesTCE
-      , stringifyMatrix,
-      progress
+      , progress
       }                   = require('./source/utils/helpers')
 
-const { getAll
-      , insert
-      , remove
-      , getByID
-      , removeAll 
+const { removeAll 
       , insertGen
-      , getRowCount
       }                   = require('./source/API/controller.db')
-const { compose } = require('lodash/fp')
+
+const { compose }         = require('lodash/fp')
 
 const { HOST
       , DATABASE
-      , PORT
+      , TELLENGTH
+      , PSSWDLENGTH
+      , HASHTAGLENGTH
       }                   = require('dotenv').config().parsed
 
                           connection.connect((e) => e ? trace(e.message) : trace(`\nMySQL DB "${DATABASE}" connect on ${HOST}.\n`))
 
                           removeAll(connection, 'users')
-
-                          const gens = fNTimes([generateMatrix, [[10000, 13], [6,9], checkDecade(10)]])
                           
-                          // gens.map((x, i) => compose(insertGen)([connection, x, i, gens.lengeh]))
+                          const gens = fNTimes([generateMatrix, [[1000000, TELLENGTH], [HASHTAGLENGTH, PSSWDLENGTH], 10]])
+
+                          trace('\n')
+                          
+                          gens.map((x, i) => compose(insertGen, progress)([connection, x, i, gens.length]))
+
+                          trace('\nNode.js finished the work.')
 
                           connection.end((e) => e ? trace(e.message) : trace(`\nMySQL DB "${DATABASE}" disconnected.\n`))
--- mysql-generator/source/utils/helpers.js
+++ mysql-generator/source/utils/helpers.js
@@ -2,42 +2,41 @@ const Progress            = require('clui').Progress
 
 const { floor }           = require('lodash')
 
-const trace               = (x, y = '') => { console.log(x, y); return x }
+const trace               = (x, y = '')     => { console.log(x, y); return x }
 
-const errorDB             = (e, x)      => e ? trace(e.message) : trace(x.message === '' ? 'Removing succeeded.' : x.message)
+const errorDB             = (e, x)          => e ? trace(e.message) : trace(x.message === '' ? 'DB cleaned.\n' : 'Add the chunk: ' + x.message)
 
-const asyncTimer          = (q, [y, z]) => { 
-                                             const x = Math.random()
-                                             console.time(`\nAsync function with hash(${x}) executed in`)
-                                             const executed = q(y, z)
-                                             return executed.then((x) => console.timeEnd(`\nAsync function with hash(${x}) executed in`))
-                                            //  return console.timeEnd(`\nAsync function with hash(${x}) executed in`)
-                                           }
+// const asyncTimer          = ([q, y, z])     => { 
+//                                                  const x = floor(Math.random(), 4)
+//                                                  console.time(`\nAsync function with hash(${x}) executed in`)
+//                                                  const executed = q(y, z)
+//                                                  return executed.then((x) => console.timeEnd(`\nAsync function with hash(${x}) executed in`))
+//                                                 //  return console.timeEnd(`\nAsync function with hash(${x}) executed in`)
+//                                                }
 
-const progress            = ([c, x, i, l]) => { trace(new Progress(20).update(floor(i/l, 1))); return [c, x] }
+const progress            = ([c, x, i, l])  => { trace(new Progress(30).update(floor((i + 1)/l, 3))); return [c, x] }
 
-const timerWrap           = (f, arg)    => { 
-                                             const x = Math.random(); 
-                                             console.time(`\nFunction with hash(${x}) executed in`); 
-                                             const executed = f(arg); 
-                                             console.timeEnd(`\nFunction with hash(${x}) executed in`);
-                                             return executed;
-                                           }
+const timerWrap           = (f, arg)        => { 
+                                                 console.time(`Generated in`)
+                                                 const executed = f(arg)
+                                                 console.timeEnd(`Generated in`)
+                                                 return executed
+                                               }
 
-const endsWithZero        = (N)         => N % 10 === 0
+const endsWithZero        = (N)             => N % 10 === 0
                                             
-const stringifyMatrix     = (x)         => x.map((y) =>`(${y[0]}, '${y[1]}', '${y[2]}')`).toLocaleString()
+const stringifyMatrix     = (x)             => x.map((y) =>`(${y[0]}, '${y[1]}', '${y[2]}')`).toLocaleString()
 
-const fNTimes             = ([f,        // execute fnc N times and write results to the array (avoid N ends with 0)
+const fNTimes             = ([f,            // execute fnc N times and write results to the array (avoid N ends with 0)
                               [[a,b],
                                [c,d],
                                N
                               ]
-                            ])          => N > 0 && !endsWithZero(N)
-                                              ? fNTimes([f, [[a,b], [c,d], N - 1]]).concat([timerWrap(f, [[a,b], [c,d], N])])
-                                                : N.toString().length > 1
-                                                  ? fNTimes([f, [[a,b], [c,d], N - 1]])
-                                                    : []
+                            ])              => N > 0 && !endsWithZero(N)
+                                                  ? fNTimes([f, [[a,b], [c,d], N - 1]]).concat([timerWrap(f, [[a,b], [c,d], N])])
+                                                    : N.toString().length > 1
+                                                      ? fNTimes([f, [[a,b], [c,d], N - 1]])
+                                                        : []
 
                           
-module.exports            = { trace, errorDB, timerWrap, asyncTimer, stringifyMatrix, fNTimes, progress }
+module.exports            = { trace, errorDB, timerWrap, stringifyMatrix, fNTimes, progress }
