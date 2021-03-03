8cc0eae542a3434f306400369dbbe9e5fc4d9ffd
lenchevskii <dima.lenchevskii@gmail.com>
2021-03-04 02:37:17 +0300

    beautify

--- mysql-generator/source/API/controller.db.js
+++ mysql-generator/source/API/controller.db.js
@@ -8,28 +8,29 @@ const { errorDB
       , shootBenchmark
       , stringifyMatrix
       }                   = require('../utils/helpers')
-const { green } = require('cli-color')
 
-const getAll              = (c)         => c.query('SELECT * FROM users', errorDB)
+const { green }           = require('cli-color')
 
-const getByID             = (c, id)     => c.query(`SELECT * FROM users WHERE id=${id}`, errorDB)
+const getAll              = (c)             => c.query('SELECT * FROM users', errorDB)
 
-const remove              = (c, id)     => c.query(`DELETE FROM users WHERE yumi_user_id=${id}`, errorDB)
+const getByID             = (c, id)         => c.query(`SELECT * FROM users WHERE id=${id}`, errorDB)
 
-const removeAll           = (c, table)  => c.query(`DELETE FROM ${table}`, errorDB)
+const remove              = (c, id)         => c.query(`DELETE FROM users WHERE yumi_user_id=${id}`, errorDB)
 
-const insertGen           = ([c, gen, id])    => new Promise((rs, rj) => { 
-                                                                          putBenchmark(id);
-                                                                          
-                                                                          rs(c.query('INSERT INTO `users` (`yumi_user_id`, `hashtag`, `password`) VALUES'
-                                                                            + `${stringifyMatrix(gen)}`,
-                                                                            (e, result)  => e ? trace(e.message)
-                                                                                                : trace(result.message === ''
-                                                                                                  ? 'DB cleaned.\n'
-                                                                                                    : 'Add generation: ' + green(result.message)
-                                                                                                      ) + shootBenchmark(id)
-                                                                                                  ))
-                                                                          })
+const removeAll           = (c, table)      => c.query(`DELETE FROM ${table}`, errorDB)
+
+const insertGen           = ([c, gen, id])  => new Promise((rs, rj) => { 
+                                                                        putBenchmark(id);
+                                                                        
+                                                                        rs(c.query('INSERT INTO `users` (`yumi_user_id`, `hashtag`, `password`) VALUES'
+                                                                          + `${stringifyMatrix(gen)}`,
+                                                                          (e, result)  => e ? trace(e.message)
+                                                                                              : trace(result.message === ''
+                                                                                                ? 'DB cleaned.\n'
+                                                                                                  : 'Add generation: ' + green(result.message)
+                                                                                                    ) + shootBenchmark(id)
+                                                                                                ))
+                                                                        })
 
 // const insertGen           = ([c, gen, id])    => new Promise((rs,rj) => {
 //                                                   putBenchmark(id);
