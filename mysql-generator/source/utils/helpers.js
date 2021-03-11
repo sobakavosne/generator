9580b76f65f383a765ac9cc2cf87f3929fdc1f7c
dzmitry <dima.lenchevskii@gmail.com>
2021-03-11 08:18:41 +0300

    test 1B contacts heap

--- mysql-generator/source/utils/helpers.js
+++ mysql-generator/source/utils/helpers.js
@@ -1,18 +1,28 @@
-const trace               = (x)                   => { console.log(x); return x }
+const { tail
+      , compose
+      , reverse
+      , isUndefined
+      }                   = require('lodash/fp')
 
-const rndmString          = (x)                   => Math.random().toString(36).substr(2, x)
+const trace               = (x)         => { console.log(x); return x }
 
-const putBenchmark        = (id)                  => id !== undefined ? console.time(`Insertion (${id}) processed in`) : []
+const rndmString          = (x)         => Math.random().toString(36).substr(2, x)
 
-const shootBenchmark      = (id)                  => id !== undefined ? console.timeEnd(`Insertion (${id}) processed in`) : []
+const checkDecade         = (N)         => N + Number(compose(reverse, tail, reverse)([...`${N}`]).join(''))
 
-const errorDB             = (e, r, id)            => e ? trace(e.message)
-                                                        : trace(r.message === ''
-                                                          ? 'DB cleaned.\n'
-                                                            : 'Add generation: ' + r.message
-                                                              ) + shootBenchmark(id)
+const endsWithZero        = (N)         => N % 10 === 0
+
+const putBenchmark        = (id)        => id !== undefined ? console.time(`Insertion (${id}) processed in`) : []
+
+const shootBenchmark      = (id)        => id !== undefined ? console.timeEnd(`Insertion (${id}) processed in`) : []
+
+const errorDB             = (e, r, id)  => e ? trace(e.message)
+                                              : trace(r.message === ''
+                                                ? 'DB cleaned.\n'
+                                                  : 'Add generation: ' + r.message
+                                                    ) + shootBenchmark(id)
 
 const stringifyMatrix     = (x)                   => x.map((y) =>`(${y[0]}, '${y[1]}', '${y[2]}')`).toLocaleString()
 
 
-module.exports            = { trace, errorDB, stringifyMatrix, putBenchmark, shootBenchmark, rndmString }
+module.exports            = { trace, errorDB, stringifyMatrix, putBenchmark, shootBenchmark, rndmString, checkDecade, endsWithZero }
